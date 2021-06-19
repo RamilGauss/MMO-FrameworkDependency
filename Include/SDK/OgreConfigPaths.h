1e2aa04213daa48b1b2114cc63be008cd797fb88
Ramil <ramil2085@mail.ru>
2021-06-19 17:02:36 +0300

    Upgraded to OGRE 1.12.12. Removed MyGUI.

---
+++ Include/SDK/OgreConfigPaths.h
@@ -0,0 +1,8 @@
+#ifndef OGRECONFIGPATHS_H_
+#define OGRECONFIGPATHS_H_
+
+#define OGRE_CONFIG_DIR "e:/Github/ogre-1.12.12/SDK/Win32/vc16_x64/SDK/bin"
+#define OGRE_MEDIA_DIR "e:/Github/ogre-1.12.12/SDK/Win32/vc16_x64/SDK/Media"
+#define OGRE_PLUGIN_DIR "e:/Github/ogre-1.12.12/SDK/Win32/vc16_x64/SDK/bin" 
+
+#endif /* OGRECONFIGPATHS_H_ */
